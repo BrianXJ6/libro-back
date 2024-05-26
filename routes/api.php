0a23d7f0079d8a5c7f53322b254cebfd82179d33
Brian Barros <brian.ferreira@pie.tax>
2024-05-26 18:58:00 -0300

    Create routes for app flow

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\EnrollmentController;
 
-Route::get('/', function () {
-    return 'oi';
+Route::get('/', fn () => 'Hello World');
+
+Route::prefix('cursos')->controller(CourseController::class)->group(function () {
+    Route::apiResources(['/' => CourseController::class]);
+    // ...
 });
+
+Route::prefix('alunos')->controller(StudentController::class)->group(function () {
+    Route::apiResources(['/' => StudentController::class]);
+    // ...
+});
+
+Route::apiResources(['matriculas' => EnrollmentController::class]);
