e5ccfbbe3a0d60bc22bd47b089e90f455f7dd4e9
Brian Barros <brian.ferreira@pie.tax>
2024-05-26 23:25:58 -0300

    fix enrollment table

--- database/migrations/2024_05_26_182626_create_enrollment_table.php
+++ database/migrations/2024_05_26_182626_create_enrollment_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('enrollment', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('student_id')->constrained()->cascadeOnDelete();
             $table->foreignId('course_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
