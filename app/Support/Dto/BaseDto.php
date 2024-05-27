6b302d7423d6c75b8a0eae7d51405f1c6dc8d5c2
Brian Barros <brian.ferreira@pie.tax>
2024-05-26 23:26:27 -0300

    Improvements in base dto for map in and out input

--- app/Support/Dto/BaseDto.php
+++ app/Support/Dto/BaseDto.php
@@ -3,7 +3,12 @@
 namespace App\Support\Dto;
 
 use Spatie\LaravelData\Data;
+use Spatie\LaravelData\Attributes\MapName;
+use Spatie\LaravelData\Attributes\MapInputName;
+use Spatie\LaravelData\Mappers\SnakeCaseMapper;
 
+#[MapName(SnakeCaseMapper::class)]
+#[MapInputName(SnakeCaseMapper::class)]
 abstract class BaseDto extends Data
 {
     /**
