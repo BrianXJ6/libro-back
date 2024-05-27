1204dd064be339351a1b40d45d6318416032c7f8
Brian Barros <brian.ferreira@pie.tax>
2024-05-27 00:25:08 -0300

    Create new unique rule for enrollment request

--- app/Http/Requests/StoreEnrollmentRequest.php
+++ app/Http/Requests/StoreEnrollmentRequest.php
@@ -4,12 +4,33 @@ namespace App\Http\Requests;
 
 use App\Models\Course;
 use App\Models\Student;
+use App\Models\Enrollment;
 use App\Dto\CrudEnrollmentDto;
 use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\ValidationException;
 
 class StoreEnrollmentRequest extends FormRequest
 {
+    private const COURSE_KEY = 'course_id';
+    private const STUDENT_KEY = 'student_id';
+
+    /**
+     * Prepare the data for validation.
+     *
+     * @return void
+     */
+    #[\Override]
+    protected function prepareForValidation(): void
+    {
+        if ($this->checkIfExist()) {
+            throw ValidationException::withMessages([
+                'messages' => __('validation.unique', ['attribute' => 'enrollment'])
+            ]);
+        }
+    }
+
     /**
      * Get the validation rules that apply to the request.
      *
@@ -31,5 +52,18 @@ class StoreEnrollmentRequest extends FormRequest
     public function getData(): CrudEnrollmentDto
     {
         return CrudEnrollmentDto::from($this->validated());
+    }
+
+    /**
+     * Checks if there is any value linked to the student and the course sent by request
+     *
+     * @return boolean
+     */
+    protected function checkIfExist(): bool
+    {
+        return DB::table((new Enrollment())->getTable())->where([
+            [self::STUDENT_KEY, $this->{self::STUDENT_KEY}],
+            [self::COURSE_KEY, $this->{self::COURSE_KEY}],
+        ])->exists();
     }
 }
